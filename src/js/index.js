6174691650b8231355b3c122ca39224ba36c62fa
hkml00 <zenxion5@gmail.com>
2023-12-13 21:42:16 +0700

    for personal portfolio

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+function redirInstagram() {
+    window.location.href = "https://instagram.com/hkml_00"
+}
+
+function redirWhatsapp() {
+    window.location.href = "https://wa.me/+6281558979519"
+}
